686e550ea114e53b223f59fef4fb0527d023b499
MetaZuo <31408703+MetaZuo@users.noreply.github.com>
2018-09-17 10:08:13 +0800

    control wait and delay threshold to be the same

--- globals.cpp
+++ globals.cpp
@@ -18,7 +18,7 @@ const int time_step = 30;
 const int max_node = 264346;
 const int max_vehicle = 1000;
 const int max_wait_sec = 360;
-const int max_delay_sec = 2 * max_wait_sec;
+const int max_delay_sec = max_wait_sec;
 const int max_capacity = 4;
 const int velocity = 84; // dm/s
 
